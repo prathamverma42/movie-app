11369bc8cbe0605900d0154f150564be72d75e21
Pratham Verma <prathamverma12345@gmail.com>
2021-05-10 23:33:26 +0530

    styling added

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,23 @@
+import React from "react";
+import { data } from "../data";
+import Navbar from "../components/Navbar";
+import MovieCard from "../components/MovieCard";
+
 function App() {
   return (
     <div className="App">
-      <h1>Project Setup </h1>{" "}
+      <Navbar />
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favourites</div>
+        </div>
+        <div className="list">
+          {data.map((movie) => {
+            return <MovieCard movie={movie} />;
+          })}
+        </div>
+      </div>
     </div>
   );
 }
