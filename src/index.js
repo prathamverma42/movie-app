bb04aa0619fd84c6bcf77fcd834c6a9d91825dd6
Pratham Verma <prathamverma12345@gmail.com>
2021-05-11 19:52:10 +0530

    currying the logger function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 
 import "./index.css";
 import App from "./components/App";
 import rootReducer from "./reducers/index";
 
-const store = createStore(rootReducer);
+// //function logger(obj, next, action)
+// // logger(obj)(next)(action)
+// const logger = function({dispatch,getState}){
+//     return function (next){
+//         return function(action){
+//             console.log('ACTION TYPE = ', action.type);
+//             next(action);
+//         }
+//     }
+// }
+
+const logger = ({dispatch, getState}) => (next) => (action) => {
+    console.log('ACTION TYPE = ', action.type);
+    next(action);
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 ReactDOM.render(<App store={store} />, document.getElementById("root"));
